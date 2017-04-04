d0f9087a3ba2aea0f58a3c1cd1f1ef9e96ae4546
leantony <chachaantony@gmail.com>
2017-04-04 13:40:41 +0300

    initial commit

---
+++ src/providers/ServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Leantony\Flash\Providers;
+
+use Laracasts\Flash\FlashServiceProvider;
+
+class ServiceProvider extends FlashServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        $this->publishes([
+            __DIR__ . '/../views' => base_path('resources/views/vendor/flash')
+        ]);
+    }
+}
---
+++ src/views/toast.blade.php
@@ -0,0 +1,24 @@
+@if (session()->has('flash_notification.message'))
+
+    <div id="notification_zone" data-message="{{ json_encode(session('flash_notification.message')) }}"
+         data-title="{{ 'Heads up:' }}" data-type="{{ session('flash_notification.level') }}" data-suppress="false">
+    </div>
+
+    <script>
+        var notif = $('#notification_zone');
+        // we have a notification
+        if (notif.length) {
+            var successTypes = ['success', 'info', 'warning'];
+            var message = notif.data('message');
+            var title = notif.data('title');
+            var type = notif.data('type');
+            var isSuccess = $.inArray(type, successTypes);
+            var suppressNotification = notif.data('suppress');
+
+            // message will always be json encoded
+            message = tenLord.utils.processMessageObject($.parseJSON(message));
+
+            tenLord.notify({text: message, type: type});
+        }
+    </script>
+@endif
